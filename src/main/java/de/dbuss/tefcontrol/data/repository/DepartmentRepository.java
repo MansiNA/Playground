28fbe0e6790ff21130473d9568b4a6e55d25b1e1
MansiNA <mansikavathiya1998@gmail.com>
2023-08-30 15:49:02 +0200

    delete department service and repository

--- src/main/java/de/dbuss/tefcontrol/data/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package de.dbuss.tefcontrol.data.repository;
-
-import de.dbuss.tefcontrol.data.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-
-}
