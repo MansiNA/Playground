aa6ee687496b7d7eb99ef80028bb6000031977d0
MansiNA <mansikavathiya1998@gmail.com>
2023-08-31 16:42:25 +0200

    added CKEditor in Defaultview

--- src/main/java/de/dbuss/tefcontrol/views/defaultPackage/DefaultView.java
+++ src/main/java/de/dbuss/tefcontrol/views/defaultPackage/DefaultView.java
@@ -1,19 +1,17 @@
 package de.dbuss.tefcontrol.views.defaultPackage;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.component.textfield.TextArea;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.router.*;
+import com.wontlost.ckeditor.Config;
+import com.wontlost.ckeditor.Constants;
+import com.wontlost.ckeditor.VaadinCKEditor;
+import com.wontlost.ckeditor.VaadinCKEditorBuilder;
 import de.dbuss.tefcontrol.data.entity.Projects;
-import de.dbuss.tefcontrol.data.service.CLTV_HW_MeasureService;
 import de.dbuss.tefcontrol.data.service.ProjectsService;
 import de.dbuss.tefcontrol.views.MainLayout;
 import jakarta.annotation.security.RolesAllowed;
@@ -23,93 +21,137 @@ import java.util.Optional;
 @PageTitle("Default Mapping")
 @Route(value = "Default-Mapping/:project_Id", layout = MainLayout.class)
 @RolesAllowed("USER")
-public class DefaultView extends VerticalLayout implements BeforeEnterObserver {
+public class DefaultView extends VerticalLayout  implements BeforeEnterObserver  {
 
     private ProjectsService projectsService;
-    private Tab descriptionTab = new Tab("Description");
-    private Tab attachmentsTab = new Tab("Attachments");
-    private Tab dbJobsTab = new Tab("DB-Jobs");
-
-    private Tab qsTab = new Tab("QS");
-
+    private TabSheet tabSheet;
+    private Button saveBtn;
+    private Button editBtn;
+    private VaadinCKEditor editor;
     private Optional<Projects> projects;
-
-    private VerticalLayout tabContentLayout = new VerticalLayout();
+    private HorizontalLayout hl;
 
     public DefaultView(ProjectsService projectsService) {
         this.projectsService = projectsService;
 
-        tabContentLayout.setSizeFull();
-        add(createTabSheet(), tabContentLayout);
-    }
+        tabSheet = new TabSheet();
+        saveBtn = new Button("save");
+        editBtn = new Button("edit");
+        saveBtn.setVisible(false);
+        editBtn.setVisible(true);
 
+        hl = new HorizontalLayout();
+        hl.add(getTabsheet());
+
+        hl.setHeightFull();
+        hl.setSizeFull();
+
+        add(hl);
+        System.out.println("constructor........");
+
+    }
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         RouteParameters parameters = event.getRouteParameters();
         String projectId = parameters.get("project_Id").orElse(null);
         if (projectId != null) {
-             projects = projectsService.findById(Long.parseLong(projectId));
+            projects = projectsService.findById(Long.parseLong(projectId));
         }
-        setSelectedTabContent(createDescriptionTabContent());
-    }
-    private Tabs createTabSheet() {
-        Tabs tabs = new Tabs(descriptionTab, attachmentsTab, dbJobsTab, qsTab);
-        tabs.setOrientation(Tabs.Orientation.HORIZONTAL);
-        tabs.addSelectedChangeListener(event -> setSelectedTabContent(getSelectedTabContent(event.getSelectedTab())));
-        return tabs;
+
+        updateComponents();
     }
 
-    private void setSelectedTabContent(Component content) {
-        tabContentLayout.removeAll();
-        tabContentLayout.add(content);
+    private void updateComponents() {
+        editor.setValue(projects.map(Projects::getDescription).orElse(""));
+    }
+    private TabSheet getTabsheet() {
+
+        //Edit-Button nur im Tab Description anzeigen:
+
+        tabSheet.addSelectedChangeListener(e->{
+
+            if (e.getSelectedTab().getLabel().contains("Description"))
+            {
+                editBtn.setVisible(true);
+            }
+            else {
+                editBtn.setVisible(false);
+            }
+
+        });
+        saveBtn.addClickListener((event -> {
+
+            projects.get().setDescription(editor.getValue());
+            projectsService.update(projects.get());
+
+            editBtn.setVisible(true);
+            saveBtn.setVisible(false);
+            editor.setReadOnly(true);
+
+        }));
+
+        editBtn.addClickListener(e->{
+            editBtn.setVisible(false);
+            saveBtn.setVisible(true);
+            editor.setReadOnly(false);
+        });
+
+            tabSheet.add("Description", getProjectsDescription());
+            tabSheet.add("Attachments", new Div(new Text("This is the Attachments tab content")));
+            tabSheet.add("DB-Jobs", new Div(new Text("This is the Job-Info/Execution tab")));
+            tabSheet.add("QS", new Div(new Text("This is the QS tab content")));
+
+        tabSheet.setSizeFull();
+        tabSheet.setHeightFull();
+
+        return tabSheet;
     }
 
-    private Component getSelectedTabContent(Tab selectedTab) {
-        if (selectedTab.equals(descriptionTab)) {
-            return createDescriptionTabContent();
-        } else if (selectedTab.equals(attachmentsTab)) {
-            return createAttachmentsTabContent();
-        } else if (selectedTab.equals(dbJobsTab)) {
-            return createDbJobsTabContent();
-        } else if (selectedTab.equals(qsTab)) {
-            return createQsTabContent();
+    private VerticalLayout getProjectsDescription() {
+
+        editBtn.setVisible(true);
+        VerticalLayout content = new VerticalLayout();
+
+        Button changeReadonlyMode = new Button("change readonly mode");
+
+        changeReadonlyMode.addClickListener((event -> {
+            editor.setReadOnlyWithToolbarAction(!editor.isReadOnly());
+        }));
+
+        Config config = new Config();
+        config.setBalloonToolBar(Constants.Toolbar.values());
+        config.setImage(new String[][]{},
+                "", new String[]{"full", "alignLeft", "alignCenter", "alignRight"},
+                new String[]{"imageTextAlternative", "|",
+                        "imageStyle:alignLeft",
+                        "imageStyle:full",
+                        "imageStyle:alignCenter",
+                        "imageStyle:alignRight"}, new String[]{});
+
+        editor = new VaadinCKEditorBuilder().with(builder -> {
+
+            builder.editorType = Constants.EditorType.CLASSIC;
+            builder.width = "95%";
+            builder.readOnly = true;
+            builder.hideToolbar=true;
+            builder.config = config;
+        }).createVaadinCKEditor();
+
+        editor.setReadOnly(true);
+
+        content.add(changeReadonlyMode,editor,editBtn,saveBtn);
+
+        if(projects != null) {
+            editor.setValue(projects.map(Projects::getDescription).orElse(""));
         }
-        return new Div(); // Default content
-    }
 
-    private VerticalLayout createDescriptionTabContent() {
-        VerticalLayout descriptionContentLayout = new VerticalLayout();
-        TextArea descriptionTextArea = new TextArea();
-        descriptionTextArea.setWidthFull();
-        if (projects != null) {
-            descriptionTextArea.setValue(projects.map(Projects::getDescription).orElse(""));
-        } else {
-            descriptionTextArea.setValue(""); // Set a default value if projects is not present
-        }
-        descriptionContentLayout.add(descriptionTextArea);
-        descriptionContentLayout.add(new Button("edit"));
-        return descriptionContentLayout;
-    }
+        //content.add(saveBtn);
 
-    private VerticalLayout createAttachmentsTabContent() {
-        VerticalLayout attachmentsContentLayout = new VerticalLayout();
-        attachmentsContentLayout.add(new Text("This is the attachments tab content."));
-        attachmentsContentLayout.add(new Button("Click Me in Attachments Tab"));
-        return attachmentsContentLayout;
-    }
+        //content.setSizeFull();
+        //content.setHeightFull();
 
-    private VerticalLayout createDbJobsTabContent() {
-        VerticalLayout dbJobsContentLayout = new VerticalLayout();
-        dbJobsContentLayout.add(new Text("This is the DB-Jobs tab content."));
-        dbJobsContentLayout.add(new Button("Click Me in DB-Jobs Tab"));
-        return dbJobsContentLayout;
-    }
+        return content;
 
-    private VerticalLayout createQsTabContent() {
-        VerticalLayout dbJobsContentLayout = new VerticalLayout();
-        dbJobsContentLayout.add(new Text("This is the QS tab content."));
-        dbJobsContentLayout.add(new Button("Click Me in QS Tab"));
-        return dbJobsContentLayout;
     }
 
 }
