7aab71fcfc7a5531c3d2f8b2c930889caa2459e3
MansiNA <mansikavathiya1998@gmail.com>
2023-10-13 14:13:31 +0200

    All Tech_KPI rows save in Database

--- src/main/java/de/dbuss/tefcontrol/data/service/ProjectConnectionService.java
+++ src/main/java/de/dbuss/tefcontrol/data/service/ProjectConnectionService.java
@@ -57,6 +57,12 @@ public class ProjectConnectionService {
         throw new RuntimeException("Database connection not found: " + selectedDatabase);
     }
 
+    public JdbcTemplate getJdbcConnection(String selectedDatabase) {
+        DataSource dataSource = getDataSource(selectedDatabase);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    }
+
     public List<CLTV_HW_Measures> fetchDataFromDatabase(String selectedDatabase) {
         DataSource dataSource = getDataSource(selectedDatabase);
         jdbcTemplate = new JdbcTemplate(dataSource);
@@ -244,14 +250,19 @@ public class ProjectConnectionService {
         }
     }
 
-    public String saveKPIFact(List<Tech_KPIView.KPI_Fact> data, String selectedDatabase) {
-
+    public void deleteKPIFact(String selectedDatabase) {
+        getJdbcConnection(selectedDatabase);
         try {
-            DataSource dataSource = getDataSource(selectedDatabase);
-            jdbcTemplate = new JdbcTemplate(dataSource);
-
             String sqlDelete = "DELETE FROM [Stage_Tech_KPI].[KPI_Fact]";
             jdbcTemplate.update(sqlDelete);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String saveKPIFact(List<Tech_KPIView.KPI_Fact> data) {
+
+        try {
 
             String sqlInsert = "INSERT INTO [Stage_Tech_KPI].[KPI_Fact] (Zeile, NT_ID, Runrate, Scenario,[Date],Wert) VALUES (?, ?, ?, ?, ?, ?)";
 
@@ -275,15 +286,18 @@ public class ProjectConnectionService {
         }
     }
 
-    public String saveKPIPlan(List<Tech_KPIView.KPI_Plan> data, String selectedDatabase) {
-
+    public void deleteKPIPlan(String selectedDatabase) {
+        getJdbcConnection(selectedDatabase);
         try {
-            DataSource dataSource = getDataSource(selectedDatabase);
-            jdbcTemplate = new JdbcTemplate(dataSource);
-
             String sqlDelete = "DELETE FROM [Stage_Tech_KPI].[KPI_Plan]";
             jdbcTemplate.update(sqlDelete);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String saveKPIPlan(List<Tech_KPIView.KPI_Plan> data) {
 
+        try {
             String sql = "INSERT INTO [Stage_Tech_KPI].[KPI_Plan] (Zeile, NT_ID, Spalte1, Scenario, VersionDate, VersionComment, Runrate) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
             jdbcTemplate.batchUpdate(sql, data, data.size(), (ps, entity) -> {
@@ -313,15 +327,18 @@ public class ProjectConnectionService {
         }
     }
 
-    public String saveKPIActuals(List<Tech_KPIView.KPI_Actuals> data, String selectedDatabase) {
-
+    public void deleteKPIActuals(String selectedDatabase) {
+        getJdbcConnection(selectedDatabase);
         try {
-            DataSource dataSource = getDataSource(selectedDatabase);
-            jdbcTemplate = new JdbcTemplate(dataSource);
-
             String sqlDelete = "DELETE FROM [Stage_Tech_KPI].[KPI_Actuals]";
             jdbcTemplate.update(sqlDelete);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String saveKPIActuals(List<Tech_KPIView.KPI_Actuals> data) {
 
+        try {
             String sqlInsert = "INSERT INTO [Stage_Tech_KPI].[KPI_Actuals] (Zeile, [NT_ID],[WTAC_ID],[sort],[M2_Area],[M1_Network],[M3_Service],[M4_Dimension],[M5_Tech],[M6_Detail],[KPI_long],[Runrate],[Unit],[Description],[SourceReport],[SourceInput],[SourceComment] ,[SourceContact] ,[SourceLink] ) VALUES (?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
             jdbcTemplate.batchUpdate(sqlInsert, data, data.size(), (ps, entity) -> {
