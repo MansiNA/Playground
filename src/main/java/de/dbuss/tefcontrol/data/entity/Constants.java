5ca24ef7f30d7de3f31649e33405393cb95fbf1c
MansiNA <mansikavathiya1998@gmail.com>
2023-11-06 16:59:54 +0100

    Added TechComment module

--- src/main/java/de/dbuss/tefcontrol/data/entity/Constants.java
+++ src/main/java/de/dbuss/tefcontrol/data/entity/Constants.java
@@ -11,6 +11,8 @@ public class Constants {
     public static final String DB_NAME = "DB_Name";
     public static final String TABLE = "Table";
 
+    public static  final String ZEILE = "zeile";
+
 
     // This constant is used for B2P_Outlook module
     public static final String B2P_OUTLOOK = "B2P_Outlook";
@@ -18,5 +20,11 @@ public class Constants {
     // This constant is used for CLTV_Inflow module
     public static final String CLTV_INFLOW = "CLTV_Inflow";
 
+
+    // This constant is used for Tech_Comments view in pbicomment module
+    public static final String PBI_TECH_COMMENTS = "PBI_Tech_Comments";
+    public static  final String XPEX = "xPEX";
+    public static  final String ITONLY = "IT only";
+    public static  final String KPIS = "KPIs";
 
 }
